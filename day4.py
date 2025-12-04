d09f7f38266246e55ca9825004480985c953d3a9
John-Progrmaer <135941360+John-Progrmaer@users.noreply.github.com>
2025-12-04 14:42:13 -0800

    Refactor Unpacker for recursive parsing and mode handling
    Refactor Unpacker class to support recursion and add a mode parameter for different processing strategies.

--- day4.py
+++ day4.py
@@ -1,60 +1,68 @@
 class Unpacker:
-    def __init__(self, source):
-        self.data = []
+    def __init__(self, source, mode=0):
+        self.data = []      # imported dataset
+        self.dupe = []      # independent clone for layering
         self.source = source
-        self.sum = 0
-        self.limit = 4
+        self.sum = 0        # final answer
+        self.limit = 4      # value defined by problem
         self.bound = 0
+        self.mode = mode    # part_1 == 0, part_2 == 1
+        self.flag = 0       # set to 1 when recursive loop is done
 
     def download(self):
+        """Extract specified source file"""
         with open(self.source, "r") as file:
             for line in file:
                 line = line.strip()
                 subsequence = []
                 for char in line:
                     subsequence.append(char)
-                self.data.append(subsequence)   
+                self.data.append(subsequence)
         self.bound = len(self.data) - 1
-        print(f"sequence created with {self.bound} rows and columns...")
-        self.parse()
+        while self.flag == 0:
+            self.parse(self.mode)
+        print(f"{self.sum}\n")
 
-    def parse(self):
+    def parse(self, loop):
+        """Iterate through each item of the dataset, recursion optional."""
+        if loop == 1:
+            self.dupe = deepcopy(self.data)
+            temp = self.sum
         i = 0
-        for row in self.data:
+        for series in self.data:
             j = 0
-            for item in row:
-                print(f"\nTesting for data surrounding {self.data[i][j]} at row {i}, column {j}...")
-                if self.data[i][j] == "@":
-                    self.sum += self.test(i, j)
-                else:
-                    print(f"point {self.data[i][j]} != @, disregard...")
+            for item in series:
+                if item == "@":
+                    check = self.test(i, j)
+                    if check == 1 and loop == 1:
+                        self.dupe[i][j] = "."
+                    self.sum += check
                 j += 1
             i += 1
-        print(self.sum)
+        if loop == 1:
+            self.data = deepcopy(self.dupe)
+            if temp == self.sum:
+                self.flag = 1
+            
 
     def test(self, row, column):
+        """For a given position, test surroundings"""
         tracker = 0
         for x in range((row - 1), (row + 2)):
             if x < 0 or x > self.bound:
-                print(f"\trow {x} outside of bounds...")
                 continue
             for y in range((column - 1), (column + 2)):
                 if y < 0 or y > self.bound:
-                    print(f"\tcolumn {y} outside of bounds...")
                     continue
                 if x == row and y == column:
                     continue
-                print(f"\trow {x}, column {y}: {self.data[x][y]}")
                 if self.data[x][y] == "@":
                     tracker += 1
-                    print(f"\t\t@ detected, tracker now at {tracker}")
-                else:
-                    print(f"\t\tclear!")
                 if tracker == self.limit:
-                    print("\ttoo many rolls, throw it out!")
                     return 0
         return 1
 
 if __name__ == "__main__":
-    tool = Unpacker("input.txt")
+    from copy import deepcopy
+    tool = Unpacker("input.txt", 1)
     tool.download()
